ecc5068fc15500e193c51b10019e4c2df876c623
YousefBadawy <125797361+YousefBadawy@users.noreply.github.com>
2023-10-28 23:39:19 +0200

    Create DMS.cpp

---
+++ DMS.cpp
@@ -0,0 +1,5 @@
+// DMS.cpp
+#include "DMS.h"
+
+DMS::DMS(double degrees, double minutes, double seconds, char direction)
+    : degrees(degrees), minutes(minutes), seconds(seconds), direction(direction) {}
